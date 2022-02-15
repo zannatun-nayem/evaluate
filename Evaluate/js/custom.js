23225e2c429e68034d3cee87da9a0f64da19c5f2
sumaiya <you@example.com>
2022-02-15 15:54:04 +0600

    first commit

---
+++ Evaluate/js/custom.js
@@ -0,0 +1,10 @@
+// animation wow js
+
+const wow = new WOW({
+  boxClass: "wow", // default
+  animateClass: "animate__animated", //custom
+  offset: 0, // default
+  mobile: true, // default
+  live: true, // default
+});
+wow.init();
